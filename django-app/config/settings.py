6365563131b3c4c76ffb747e049efbf38861357a
JeongEuiJin <hehar1020@gmail.com>
2017-06-12 14:18:14 +0900

    commentlike model add

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'post',
-    'django-extensions'
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
--- django-app/post/models.py
+++ django-app/post/models.py
@@ -9,12 +9,33 @@ class Post(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
     like_users = models.ManyToManyField(
         User,
-        related_name='like_posts'
+        related_name='like_posts',
+        through='PostLike',
     )
     tags = models.ManyToManyField(
         'Tag',
 
     )
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)  # get_or_create가 머지..
+        if not self.tags.filter(name=tag_name).exists():
+            self.tags.add(tag)
+    @property
+    def like_count(self):
+        return self.like_user.conut()
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'post_post_like_users'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
@@ -22,9 +43,18 @@ class Comment(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_comments',
+        through='CommentLike',
+    )
 
-class PostLike(models.Model):
-    pass
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    create_date = models.DateTimeField(auto_now_add=True)
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
