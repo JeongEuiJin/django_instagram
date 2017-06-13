5ae8b710d84da3a5c71470311210e20a2f1f3c04
JeongEuiJin <hehar1020@gmail.com>
2017-06-13 15:38:58 +0900

    postlist, postdetail에 css적용 및 include사용

--- django-app/config/settings.py
+++ django-app/config/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
