17242f84fce6887168795fde5f172cdca4391ddd
JeongEuiJin <hehar1020@gmail.com>
2017-06-15 14:41:44 +0900

    login templates css modfiy

--- django-app/post/urls.py
+++ django-app/post/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    url(r'^$',views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
     # url(r'^(\d+)/$',views.post_detail),
-    url(r'^(?P<post_pk>\d+)/$',views.post_detail,name='post_detail'),
-    url(r'^create/$',views.post_create,name='post_create'),
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^create/$', views.post_create, name='post_create'),
 
 ]
